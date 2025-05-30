cbdf30ce4f87ac3d5db702c8bfebadfcb8eee4be
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2025-05-30 02:19:29 +0200

    Data preparation and attempt of a predict

--- data_load.py
+++ data_load.py
@@ -5,24 +5,27 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 from sodapy import Socrata
-import datetime
+from datetime import *
+import pytz
 
 # @title Obtaining data from Vilanova i la Geltrú (last execution lasted one minute)
-def download_data(since:   str  = '2022-01-01T00:00:00',
+def download_data(since:   str  = '2020-01-01T00:00:00',
                   url:     str  = "analisi.transparenciacatalunya.cat",
                   codes:   list = ['YR'],
                   cod_var: list = ['32','40','42','3','33','44','2','34','31','51','1','30','50']):
     try:
-        df = pd.read_csv('data.csv')
-        return df
+        if since == '2020-01-01T00:00:00':
+            df = pd.read_csv('data.csv')
+            return df
     except Exception as e:
         print(f'Unable to read from file.')
     try:
-        df = pd.read_csv('unprocessed.csv')
-        return df
+        if since == '2020-01-01T00:00:00':
+            df = pd.read_csv('unprocessed.csv')
+            return df
     except Exception as e:
         print(f'Unable to read unprocessed file.')
-
+    print('\nDownloading data from open data source...')
     with Socrata(url, None, timeout=6000) as client:
         # Build a string of station codes wrapped in double quotes
         codes_str = ', '.join(f'"{c}"' for c in codes)
@@ -60,17 +63,17 @@ def download_data(since:   str  = '2022-01-01T00:00:00',
 def process_data(df: pd.DataFrame):
     # @title Preprocessing of the data
     try:
+        print(df.head())
         df = df.drop_duplicates(subset=['id'])
 
         grouped = df.groupby('data_lectura')[['codi_variable', 'valor_lectura']].apply(lambda x: x)
         dates = [index[0] for index in grouped.index]
         dates = list(set(dates))
         print(dates[0])
-        print(grouped.loc['2023-05-01T00:00:00.000'].to_numpy())
         format = bool(datetime.fromisoformat(dates[0]))
 
-
-        new_df = pd.DataFrame(columns=['DATA', 'HR', 'HRn', 'HRx', 'P', 'Pn', 'T', 'Tn', 'Tx'])
+        new_df = pd.DataFrame(columns=['DATA', 'HR', 'HRn', 'HRx', 'P', 'Pn', 'T', 'Tn', 'Tx', 'Px',
+                                       'DV10', 'DVVx10', 'VV10', 'VVx10'])
         for date in dates:
             row = grouped.loc[date].to_numpy()
             # For reference: value[0] = CODI_VARIABLE & value[1] = VALOR_LECTURA
@@ -97,11 +100,12 @@ def process_data(df: pd.DataFrame):
         new_df.drop("Unnamed: 0",   axis=1, inplace=True, errors='ignore')
         new_df.drop("Unnamed: 0.1", axis=1, inplace=True, errors='ignore')
         if format is False:
-            new_df['DATA'] = new_df['DATA'].apply(lambda x: datetime.datetime.strptime(x, "%d/%m/%Y %H:%M").isoformat(timespec='seconds'))
+            new_df['DATA'] = new_df['DATA'].apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M").isoformat(timespec='seconds'))
         new_df.apply(pd.to_numeric, errors='ignore')
     except Exception as e:
         print(f"Unable to process data. Don't worry, it might have been processed before. Error: {e}")
         new_df = df
+    new_df['DATA'] = pd.to_datetime(new_df['DATA'])
     new_df.dropna(inplace=True)
     new_df.set_index('DATA', inplace=True)
     new_df.sort_index(inplace=True)
@@ -109,37 +113,52 @@ def process_data(df: pd.DataFrame):
     new_df.to_csv('data.csv')
     return new_df
 
-def get_latest_influxdb(url: str, token: str, org: str):
-    query = """
+def prepare_for_predict(df: pd.DataFrame, entries: int = 2*48) -> pd.DataFrame:
+    new_df = df.copy()
+    last_index = df.index[-1]
+    # empty_data = {col: ['' for _ in range(entries)] for col in df.columns}
+
+    # for i in range(entries):
+    #     time = datetime.fromisoformat(last_index)
+    #     time = time + timedelta(minutes=30*(i+1))
+    #     time = time.isoformat(timespec='milliseconds')
+    #     print(time)
+    #     row = pd.DataFrame(index=[time], columns=['HR', 'HRn', 'HRx', 'P', 'Pn', 'T', 'Tn', 'Tx',
+    #                                               'Px', 'DV10', 'DVVx10', 'VV10', 'VVx10'])
+    #     new_df = pd.concat([new_df, row], ignore_index=False)
+    # print(f'Before shifting:\n{new_df.iloc[(len(new_df)-entries):len(new_df)]}\n')
+    new_df = new_df.shift(periods=entries, freq='30min')
+    # print(f'After shifting:\n{new_df.iloc[(len(new_df)-entries):len(new_df)]}')
+    return new_df
+
+def get_latest_influxdb(url: str, token: str, org: str, type: str = 'real') -> datetime:
+    query = f"""
     from(bucket:"SMAC-EK")
         |> range(start: 2020-01-01T00:00:00Z)
         |> filter(fn:(r) => r._measurement == "Meteocat")
         |> filter(fn:(r) => r._field == "temperature")
-        |> filter(fn:(r) => r._type  == "real")
+        |> filter(fn:(r) => r._type  == "{type}")
         |> last()
     """
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
     query_api = client.query_api()
     result = query_api.query(org=org, query=query)
 
-    results = []
-    for table in result:
-        for record in table.records:
-            results.append(record.get_time())
-    return results
+    if len(result) == 0:
+        return None
+    return result[0].records[0].get_time()
 
-def upload_to_influxdb(url: str, token: str, org: str, df: pd.DataFrame):
+def upload_to_influxdb(url: str, token: str, org: str, df: pd.DataFrame, type: str = 'real'):
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
     points = []
     for index, row in df.iterrows():
-        time = datetime.datetime.fromisoformat(index)
-        time = time - datetime.timedelta(hours=2)
+        # time = datetime.fromisoformat(index)
+        time = index - timedelta(hours=2)
         time = time.isoformat(timespec='seconds')
-        print(f'{time}Z')
         p = (influxdb_client.Point("Meteocat")
-             .tag("_type", "real")
+             .tag("_type", f"{type}")
              .field("temperature", row['T'])
              .time(f'{time}Z'))
         points.append(p)
--- predict.py
+++ predict.py
@@ -1,10 +1,16 @@
 import pickle
-import influxdb_client, os, time, sys, socket, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+import os
+import sys
+
 from dotenv import load_dotenv
-from sklearn.linear_model import *
 from data_load import *
+
+from sklearn.linear_model import *
+from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
 
 load_dotenv()
 token = os.getenv("INFLUXDB_TOKEN")
@@ -12,6 +18,56 @@ org = "EPSEVG" #your organization
 url = "http://localhost:8086"
 bucket = "SMAC-EK" #your bucket
 
+def plot_test_pred(y_test, y_pred):
+    # Let's take ONLY 0.2% of the predicted and test data to plot it.
+    # If we try to plot everything, it's gonna take some while...
+    _, y_test_plot, _, y_pred_plot = train_test_split(y_test, y_pred,
+                                                      test_size=1,
+                                                      random_state=1,
+                                                      shuffle=False)
+    test_index = y_test_plot.index
+
+    plt.figure(figsize=(20,5))
+
+    plt.plot(test_index, y_test_plot,
+            label='Actual (Test)', marker='o')
+    plt.plot(test_index, y_pred_plot,
+            label='Predicted', marker='x', color='red')
+
+    plt.title("Comparison of Actual vs. Predicted Temperatures (Test Set)")
+    plt.xticks(np.arange(len(test_index)), test_index, rotation='vertical')
+    plt.xlabel("Date/Time (Test)")
+    plt.ylabel("Temperature (Celsius)")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def predict(model: Lasso, df: pd.DataFrame):
+    # df['temp_prev'] = df['T'].shift(2*24)
+    df = df.dropna()
+    print(df)
+    X = df.drop(['T', 'Tn', 'Tx'], axis=1)
+    y = df['T']
+
+    scaler = preprocessing.StandardScaler().fit(X)
+    X = scaler.transform(X)
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20,
+    #                                                     random_state=1, shuffle=False)
+    # model.fit(X_train, y_train)
+
+    y_pred = model.predict(X)
+
+    mae = mean_absolute_error(y, y_pred)
+    r2  = r2_score(y, y_pred)
+
+    print("Mean Absolute Error (MAE):", mae)
+    print("R^2 Score:", r2)
+
+    plot_test_pred(y, y_pred)
+
+    return y_pred
 
 def main():
     try:
@@ -20,15 +76,26 @@ def main():
     except Exception as e:
         print(f'Error loading model -> {e}')
         return -1
+
     latest = get_latest_influxdb(url, token, org)
-    if len(latest) == 0:
-        df = download_data(since='2020-01-01T00:00:00')
+    if latest is None:
+        print('Getting latest data since 2020...')
+        df = download_data()
     else:
-        df = download_data(since=latest[0])
+        print(f'Getting latest data since {latest.isoformat()}')
+        df = download_data(since=latest.isoformat().split('+')[0])
+
     df.to_csv('unprocessed.csv')
     df = process_data(df)
     print(df.head())
     upload_to_influxdb(url, token, org, df)
+
+    new_df = prepare_for_predict(df)
+
+    y_pred = predict(model, new_df)
+
+    upload_to_influxdb(url, token, org, y_pred, 'predicted')
+
     print(f'Successfully uploaded the data')
 
 if __name__ == '__main__':
